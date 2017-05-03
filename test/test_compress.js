a3e087e58ae4d9dcc5ee511d6ad9e948f238b87f
Tyler Crumpton <tyler.crumpton@gmail.com>
2017-05-03 13:45:09 -0500

    Change 4-tabs to 2-tabs.

--- test/test_compress.js
+++ test/test_compress.js
@@ -2,54 +2,55 @@ var should = require('chai').should();
 var expect = require('chai').expect;
 var compress = require('../lib/compress');
 
-describe('compress', function() {
-    it('should throw range error if provided too few bytes', function() {
-        // Setup
-        var bytes = [1, 2, 3, 4, 5];
-        var words = 6;
+describe('compress', function () {
+  it('should throw range error if provided too few bytes', function () {
+    // Setup
+    var bytes = [1, 2, 3, 4, 5];
+    var words = 6;
 
-        // Act
-        var badCompress = function() {
-            compress(bytes, words);
-        }
+    // Act
+    var badCompress = function () {
+      compress(bytes, words);
+    }
 
-        // Assert
-        expect(badCompress).to.Throw(RangeError);
-    });
 
-    it('should work when length is equal to words', function() {
-        // Setup
-        var bytes = [1,2,3,4,5];
-        var words = 5;
+    // Assert
+    expect(badCompress).to.Throw(RangeError);
+  });
 
-        // Act
-        var result = compress(bytes, words);
+  it('should work when length is equal to words', function () {
+    // Setup
+    var bytes = [1, 2, 3, 4, 5];
+    var words = 5;
 
-        // Assert
-        expect(result).to.deep.equal([1,2,3,4,5]);
-    });
+    // Act
+    var result = compress(bytes, words);
 
-    it('should work when length is multiple of words', function() {
-        // Setup
-        var bytes = [1,2,2,1,2,2];
-        var words = 3;
+    // Assert
+      expect(result).to.deep.equal([1, 2, 3, 4, 5]);
+  });
 
-        // Act
-        var result = compress(bytes, words);
+  it('should work when length is multiple of words', function () {
+    // Setup
+    var bytes = [1, 2, 2, 1, 2, 2];
+    var words = 3;
 
-        // Assert
-        expect(result).to.deep.equal([3, 3, 0]);
-    });
+    // Act
+    var result = compress(bytes, words);
 
-    it('should work when length is not a multiple of words', function() {
-        // Setup
-        var bytes = [1,3,2,4,2,2,3];
-        var words = 3;
+    // Assert
+      expect(result).to.deep.equal([3, 3, 0]);
+ });
 
-        // Act
-        var result = compress(bytes, words);
+  it('should work when length is not a multiple of words', function () {
+    // Setup
+    var bytes = [1, 3, 2, 4, 2, 2, 3];
+    var words = 3;
 
-        // Assert
-        expect(result).to.deep.equal([2, 6, 3]);
-    });
+    // Act
+    var result = compress(bytes, words);
+
+    // Assert
+      expect(result).to.deep.equal([2, 6, 3]);
+  });
 });
